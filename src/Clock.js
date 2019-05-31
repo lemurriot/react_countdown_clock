caee061a1c6a23f28c47b247c0ae1dd49935e051
Kevin Boyle <papershack@gmail.com>
2019-05-30 20:52:57 -0700

    add functionality for clock to auto-update

--- src/Clock.js
+++ src/Clock.js
@@ -12,13 +12,26 @@ class Clock extends Component {
         }
     }
 
+    componentWillMount(){
+        this.getTimeUntil(this.props.deadline);
+    }
+
+    componentDidMount(){
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+    
     getTimeUntil(deadline){
         const time = Date.parse(deadline) - Date.parse(new Date());
-        console.log(time);
+        const seconds = Math.floor((time/1000)%60);
+        const minutes = Math.floor((time/1000/60)%60);
+        const hours = Math.floor(time/(1000*60*60)%24);
+        const days = Math.floor(time/(1000*60*60*24));
+        
+        this.setState({ days, hours, minutes, seconds})
     }
     render(){
         const { days, hours, minutes, seconds } = this.state;
-        this.getTimeUntil(this.props.deadline);
+
         return (
             <section className="clock-container">
                 <div className="clock clock-days">{days} Days</div>
